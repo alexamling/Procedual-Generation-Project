54f571e9b2d74e6b0701fbdbc484b1eaba1294e0
Alexander Amling <aja7073@rit.edu>
2018-08-01 22:22:26 -0400

    Basic Setup and Start to Project
    Created project
    
    Added a Noise Generator which returns a noisemap from perlin values
    Added a Map Display which Displays the noisemap on a plane
    Added a Map Generator to control the process
    Added a custom editor to the Map Generator to make it auto update and generate outside of the scene playing

---
+++ Procedural Generation Project/Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGenerator = (MapGenerator)target;
+
+        if (DrawDefaultInspector())
+        {
+            if (mapGenerator.autoUpdate)
+            {
+                mapGenerator.Generate();
+            }
+        }
+
+        if (GUILayout.Button("Generate"))
+        {
+            mapGenerator.Generate();
+        }
+    }
+}
---
+++ Procedural Generation Project/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+    public int mapWidth;
+    public int mapheight;
+    public float noiseScale;
+
+    public bool autoUpdate;
+
+    public void Generate()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapheight, noiseScale);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+    }
+}
